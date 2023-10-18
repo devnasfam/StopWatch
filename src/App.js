6e3bab3a2d739c3cc2c6b792c77a9d6933d48516
Dev Nasfam <90545069+devnasfam@users.noreply.github.com>
2023-10-18 15:09:04 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TimeProvider, TimeContext } from './TimeContext'/;
+import './style.scss';
+import Stopwatch from './StopWatch';
+
+const App = () =>{
+    return(
+       <TimeProvider>
+        <StopWatch />
+       </TimeProvider>
+    );
+}
+expor default App;
